5a0fb1b724aa25c749b8f13484c6cbc4fdc3d6e6
linckandrea <linckandrea01@gmail.com>
2018-09-25 21:22:37 +0200

    Victara: Add state-notifier and enable it

--- drivers/input/touchscreen/atmel_mxt_ts_mmi.c
+++ drivers/input/touchscreen/atmel_mxt_ts_mmi.c
@@ -32,6 +32,10 @@
 #include "touchx.h"
 #endif
 
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
+
 enum {
 	STATE_UNKNOWN,
 	STATE_ACTIVE,
@@ -2510,7 +2514,7 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 	case STATE_UNKNOWN:
 	case STATE_FLASH:
 		/* no special handling for these states */
-			break;
+		break;
 
 	case STATE_SUSPEND:
 		if (!data->mode_is_wakeable)
@@ -2518,7 +2522,10 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 		data->enable_reporting = false;
 		if (!data->in_bootloader)
                          mxt_sensor_state_config(data, SUSPEND_IDX);
-			break;
+#ifdef CONFIG_STATE_NOTIFIER
+		state_suspend();
+#endif
+		break;
 
 	case STATE_ACTIVE:
 		if (!data->in_bootloader)
@@ -2530,18 +2537,21 @@ static void mxt_set_sensor_state(struct mxt_data *data, int state)
 			mxt_restore_default_mode(data);
 			pr_debug("Non-persistent mode; restoring default\n");
 		}
-			break;
+#ifdef CONFIG_STATE_NOTIFIER
+		state_resume();
+#endif
+		break;
 
 	case STATE_STANDBY:
 		mxt_irq_enable(data, false);
-			break;
+		break;
 
 	case STATE_BL:
 		if (!data->in_bootloader)
 			data->in_bootloader = true;
 
 		mxt_irq_enable(data, false);
-			break;
+		break;
 
 	case STATE_INIT:
 		/* set flag to avoid object specific message handling */
